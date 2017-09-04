1f7fbd09fa2ec910722fc919e7a60d1312d3d6c4
Antonio <roberio.roberiocrato@gmail.com>
2017-09-03 22:13:58 -0300

    Login via API Google GMAIl concluído

--- app/src/main/java/com/des/odontec/equipe/odontec/View/MainActivity_Login.java
+++ app/src/main/java/com/des/odontec/equipe/odontec/View/MainActivity_Login.java
@@ -21,10 +21,10 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiActivity;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,6 +33,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.Arrays;
 
@@ -69,6 +70,7 @@ public class MainActivity_Login extends AppCompatActivity implements GoogleApiCl
         callbackManager = CallbackManager.Factory.create();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -133,8 +135,7 @@ public class MainActivity_Login extends AppCompatActivity implements GoogleApiCl
         loginGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-                startActivityForResult(signInIntent, RC_SIGN_IN);
+                loginGoogle();
             }
         });
 
@@ -159,19 +160,40 @@ public class MainActivity_Login extends AppCompatActivity implements GoogleApiCl
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==RC_SIGN_IN){
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
+            GoogleSignInAccount account = result.getSignInAccount();
+            firebaseAuthWithGoogle(account);
 
+        }else{
+            callbackManager.onActivityResult(requestCode, resultCode, data);
         }
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+
     }
 
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential= GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        aut.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser us=aut.getCurrentUser();
+                    logar(us);
+                    Toast.makeText(MainActivity_Login.this,"Seja bem vindo",Toast.LENGTH_SHORT).show();
+                    finish();
+                }else{
+                    Toast.makeText(MainActivity_Login.this,"Erro ao logar",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+/*
     private void handleSignInResult(GoogleSignInResult result) {
         if(result.isSuccess()){
             Toast.makeText(MainActivity_Login.this,"sucesso",Toast.LENGTH_SHORT).show();
+            //chama o método de login
         }else{
             Toast.makeText(MainActivity_Login.this,"Erro ao logar",Toast.LENGTH_SHORT).show();
         }
-    }
+    } */
 
     private void loginFacebook(){
         LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile","user_friends","email"));
@@ -179,13 +201,12 @@ public class MainActivity_Login extends AppCompatActivity implements GoogleApiCl
     }
 
     private void facebooktToken(AccessToken accessToken){
-        final FirebaseAuth auten= ConfiguracaoFirebase.autenticarDados();
         AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
-        auten.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        aut.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    FirebaseUser us=auten.getCurrentUser();
+                    FirebaseUser us=aut.getCurrentUser();
                     logar(us);
                     Toast.makeText(MainActivity_Login.this,"Seja bem vindo",Toast.LENGTH_SHORT).show();
                     finish();
@@ -199,7 +220,8 @@ public class MainActivity_Login extends AppCompatActivity implements GoogleApiCl
     }
 
     private void loginGoogle(){
-
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
 
     }
 
@@ -247,6 +269,6 @@ public class MainActivity_Login extends AppCompatActivity implements GoogleApiCl
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Toast.makeText(MainActivity_Login.this,"Erro ao logar"+connectionResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
     }
 }
